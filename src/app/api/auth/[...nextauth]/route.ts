f5ed65f616bc852183246b4cdef645ff1649ccf4
umartils <umartilmisani48@gmail.com>
2026-05-24 20:56:37 +0700

    remove prisma adapter

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,14 +1,14 @@
 import NextAuth from "next-auth"
 import CredentialsProvider from "next-auth/providers/credentials"
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import { prisma } from "@/lib/prisma"
-import bcrypt from "bcrypt"
+import { prisma } from "@/lib/prisma";
+import bcrypt from "bcrypt";
 
 const handler = NextAuth({
-  adapter: PrismaAdapter(prisma),
+  // adapter: PrismaAdapter(prisma),
 
   session: {
-    strategy: "database",
+    strategy: "jwt",
   },
 
   providers: [
