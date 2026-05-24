3ff036d06187081004f7f5fabc0fd7d2b7d6a4d3
umartils <umartilmisani48@gmail.com>
2026-05-24 19:04:35 +0700

    update auth.ts

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,7 +2,7 @@ import NextAuth from "next-auth";
 
 import Credentials from "next-auth/providers/credentials";
 
-import { PrismaAdapter } from "@auth/prisma-adapter";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
 
 import bcrypt from "bcrypt";
 
